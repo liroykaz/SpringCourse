85fc986d12cef12d9df5547592bcd1cf0f7eca54
lebedev <liroy63@mail.ru>
2021-02-25 15:51:30 +0400

    Добавлен фронт для приложения. Создан базовый SpringBoot проект

---
+++ BookShopSpringBootApp/src/main/java/com/bookshop/demo/BookShopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookShopSpringBootApplication.class, args);
+    }
+
+}
